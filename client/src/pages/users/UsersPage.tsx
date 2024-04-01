4677bebaed863b93deb7589c026b90dbaa01263f
habib16gb <habib16gb@gmail.com>
2024-04-01 19:15:17 +0100

    add api backend and add user resource

--- client/src/pages/users/UsersPage.tsx
+++ client/src/pages/users/UsersPage.tsx
@@ -1,19 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import {  Outlet } from "react-router-dom";
-import axios from 'axios'
+import { Outlet } from "react-router-dom";
+import axios from "axios";
 import MuiDataGrid from "../../components/mui/MuiDataGrid";
 
 const UsersPage = () => {
   const [users, setUsers] = useState([]);
-  const url = 'https://dummyjson.com/users'
 
-  useEffect(() => {
-    axios.get(url).then((res) => {
-      console.log(res.data.users[1])
-      setUsers(res.data.users)
-    }).catch((err) => console.error(err.message))
-  },[])
   return (
     <Box>
       <Typography variant='h4' color='initial'>
