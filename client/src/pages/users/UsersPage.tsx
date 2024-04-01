d0d1913097d243e1d8549e3dc2434830ce5c0cc2
habib16gb <76877928+habib16gb@users.noreply.github.com>
2024-04-01 13:32:30 +0000

    add muidatagrid

--- client/src/pages/users/UsersPage.tsx
+++ client/src/pages/users/UsersPage.tsx
@@ -1,38 +1,25 @@
-import { Box, Typography, Button, ButtonGroup } from "@mui/material";
-import { Link, Outlet } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import {  Outlet } from "react-router-dom";
+import axios from 'axios'
+import MuiDataGrid from "../../components/mui/MuiDataGrid";
 
 const UsersPage = () => {
+  const [users, setUsers] = useState([]);
+  const url = 'https://dummyjson.com/users'
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      console.log(res.data.users[1])
+      setUsers(res.data.users)
+    }).catch((err) => console.error(err.message))
+  },[])
   return (
     <Box>
       <Typography variant='h4' color='initial'>
         users page
       </Typography>
-      <ButtonGroup variant='text' color='primary' aria-label=''>
-        <Button
-          sx={{ margin: "1rem" }}
-          size='small'
-          variant='contained'
-          color='secondary'
-        >
-          <Link to={"/"}>back home page</Link>
-        </Button>
-        <Button
-          sx={{ margin: "1rem" }}
-          size='small'
-          variant='contained'
-          color='success'
-        >
-          <Link to={"/users/add"}>add new user</Link>
-        </Button>
-        <Button
-          sx={{ margin: "1rem" }}
-          size='small'
-          variant='contained'
-          color='success'
-        >
-          <Link to={"/users/update"}>update user</Link>
-        </Button>
-      </ButtonGroup>
+      <MuiDataGrid />
       <Outlet />
     </Box>
   );
